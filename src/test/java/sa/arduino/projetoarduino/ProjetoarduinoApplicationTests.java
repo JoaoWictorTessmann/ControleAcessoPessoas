b7fbca453146e44543ced9574b9a5e38c4f938f8
João Wictor <joaowictortessmann97@gmail.com>
2026-05-02 17:46:32 -0300

    Adicionar estrutura basica do Spring Boot

---
+++ src/test/java/sa/arduino/projetoarduino/ProjetoarduinoApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.arduino.projetoarduino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoarduinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
