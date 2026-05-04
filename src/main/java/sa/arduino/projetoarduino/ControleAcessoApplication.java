9374383f0e196d7c743c6fcf35b88c3de6cb904f
João Wictor <joaowictortessmann97@gmail.com>
2026-05-04 20:51:58 -0300

    chore: atualiza classe principal da aplicação

--- src/main/java/sa/arduino/projetoarduino/ControleAcessoApplication.java
+++ src/main/java/sa/arduino/projetoarduino/ControleAcessoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoarduinoApplication {
+public class ControleAcessoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoarduinoApplication.class, args);
+		SpringApplication.run(ControleAcessoApplication.class, args);
 		System.out.println("Arduino Application started successfully.");
 		System.out.println("Access aplication at: http://localhost:8080/");
 	}
